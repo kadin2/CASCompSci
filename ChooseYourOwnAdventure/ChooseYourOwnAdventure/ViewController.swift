7933e1befeb7fcd299abf42db5e74a1baef275e9
kadin2 <54858099+kadin2@users.noreply.github.com>
2019-09-27 10:15:08 -0700

    This is my Create Your Own Adventure Project.

---
+++ ChooseYourOwnAdventure/ChooseYourOwnAdventure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChooseYourOwnAdventure
+//
+//  Created by Kadin Mesriani on 9/27/19.
+//  Copyright © 2019 Kadin Mesriani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
