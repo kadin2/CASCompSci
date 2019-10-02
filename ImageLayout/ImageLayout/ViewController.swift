346e2bd88146719105ddd70add86dae87aeb06bf
kadin2 <54858099+kadin2@users.noreply.github.com>
2019-10-02 09:13:57 -0700

    The Good Instagramish

---
+++ ImageLayout/ImageLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageLayout
+//
+//  Created by Kadin Mesriani on 10/2/19.
+//  Copyright © 2019 Kadin Mesriani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
